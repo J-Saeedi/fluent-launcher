2a20ec831b80e857689106b169b6499473c25675
J-Saeedi <j.saeedi.edu@gmail.com>
2024-07-12 22:19:15 +0330

    add gpgpu support

--- fluent-launcher.py
+++ fluent-launcher.py
@@ -29,11 +29,11 @@ def write_journal(path, input_case, output_case, iteration=1500):
                 .replace('#OUTPUT', output_case))
 
 
-def run_fluent(path, fluent_path, core: int = 1):
+def run_fluent(path, fluent_path, core: int = 1, gpu=0):
     main_dir = os.getcwd()
     os.chdir(path)
     p = subprocess.call(
-        [fluent_path, '3d', f'-t{core}', '-g', '-i', './runner.jou'],  shell=True, close_fds=True)
+        [fluent_path, '3d', f'-t{core}', '-g', '-i', './runner.jou', f"-gpgpu={gpu}"],  shell=True, close_fds=True)
     # p.wait()
     # p.kill()
     # os.system(
@@ -41,7 +41,7 @@ def run_fluent(path, fluent_path, core: int = 1):
     os.chdir(main_dir)
 
 
-def main(input_name: str, output_name: str, max_iter: int, core: int, fluent_path: str, case_file: str):
+def main(input_name: str, output_name: str, max_iter: int, core: int, fluent_path: str, case_file: str, gpu: str):
     case_list = read_case_list(case_file)
     s = CheckStatus()
     current_dir = os.getcwd()
@@ -50,7 +50,8 @@ def main(input_name: str, output_name: str, max_iter: int, core: int, fluent_pat
             os.chdir(current_dir)
             s.write_status(path, StatusType.started)
             write_journal(path, input_name, output_name, max_iter)
-            run_fluent(path, core=core, fluent_path=fluent_path)
+            run_fluent(path, core=core, fluent_path=fluent_path,
+                       gpu=gpu)
             s.write_status(path, StatusType.finished)
 
 
@@ -80,8 +81,13 @@ if __name__ == "__main__":
                         default='C:/Program Files/ANSYS Inc/v212/fluent/ntbin/win64/fluent.exe',
                         help="custom location of fluent.exe")
 
+    parser.add_argument('--gpgpu', type=int, required=False,
+                        nargs='?', const=1,
+                        default=0,
+                        help="number of gpu")
+
     args = parser.parse_args()
 
     main(input_name=args.input, output_name=args.output,
          max_iter=args.max_iter, core=args.core,
-         case_file=args.case_list, fluent_path=args.fluent)
+         case_file=args.case_list, fluent_path=args.fluent, gpu=args.gpgpu)
